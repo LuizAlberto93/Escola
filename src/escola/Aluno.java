f3d85e6c594b087ff8b48dc8d3d5df17c9a27337
Luiz Alberto <costaesilva93@outlook.com>
2020-10-26 11:36:57 -0300

    criada a classe main

--- src/escola/Aluno.java
+++ src/escola/Aluno.java
@@ -1,55 +1,76 @@
 package escola;
 
-
+import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.ResolverStyle;
+import java.util.Arrays;
 
-public class Aluno extends Calculavel implements Comparable<Aluno> {
+public class Aluno implements Comparable<Aluno> {
+	private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+			.withResolverStyle(ResolverStyle.STRICT);
 
-		private String nome;
-		private int num_matricula;
-		private LocalDate data_de_ingresso;
-		private int numero_materias;
-		
-		public int compareTo(Aluno aluno) {	
-			if (aluno.getData_de_ingresso().isAfter(this.getData_de_ingresso())) {
-				return 1;
-			} else if (aluno.getData_de_ingresso().isBefore(this.getData_de_ingresso())) {
-				return -1;
-			} else {
-				return 0;
-			}
-		}
-		
-		//getters and setters
-		public int getNumero_materias() {
-			return numero_materias;
-		}
+	private String nome;
+	private int num_matricula;
+	private LocalDate data_de_ingresso;
 
-		public void setNumero_materias(int numero_materias) {
-			this.numero_materias = numero_materias;
-		}
+	public Aluno() {
 
-		public void setData_de_ingresso(LocalDate data_de_ingresso) {
-			this.data_de_ingresso = data_de_ingresso;
-		}
+	}
 
-		public String getNome() {
-			return nome;
-		}
+	public Aluno(String nome, int num_matricula, LocalDate data_de_ingresso) {
+		super();
+		this.nome = nome;
+		this.num_matricula = num_matricula;
+		this.data_de_ingresso = data_de_ingresso;
+	}
 
-		public void setNome(String nome) {
-			this.nome = nome;
+	public int compareTo(Aluno aluno) {
+		if (aluno.getData_de_ingresso().isAfter(this.getData_de_ingresso())) {
+			return 1;
+		} else if (aluno.getData_de_ingresso().isBefore(this.getData_de_ingresso())) {
+			return -1;
+		} else {
+			return 0;
 		}
+	}
 
-		public int getNum_matricula() {
-			return num_matricula;
-		}
+	public <T extends Comparable<Aluno>> void ordenarVetor(T vet[]) {
+		Arrays.sort(vet);
+	}
 
-		public void setNum_matricula(int num_matricula) {
-			this.num_matricula = num_matricula;
-		}
+	// getters and setters
 
-		public LocalDate getData_de_ingresso() {
-			return data_de_ingresso;
-		}
+	public void setData_de_ingresso(LocalDate data_de_ingresso) {
+		this.data_de_ingresso = data_de_ingresso;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getNum_matricula() {
+		return num_matricula;
+	}
+
+	public void setNum_matricula(int num_matricula) {
+		this.num_matricula = num_matricula;
+	}
+
+	public LocalDate getData_de_ingresso() {
+		return data_de_ingresso;
+	}
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome 
+				+ ", num_matricula=" 
+				+ num_matricula + ", data_de_ingresso="
+				+ data_de_ingresso.format(formatter) + "]";
+	}
+
 }
--- src/escola/Application.java
+++ src/escola/Application.java
@@ -1,5 +1,34 @@
 package escola;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Application {
+	static Aluno[] aluno;
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+
+		aluno = new Aluno[8];
+
+		for (int i = 0; i < 2; i++) {
+
+			in.nextLine();
+			System.out.println("Nome do aluno " + (i + 1) + ": ");
+			String nome = in.nextLine();
+
+			System.out.println("Matricula: ");
+			int matricula = in.nextInt();
+
+			System.out.println("Data de Ingresso: ");
+			String dataDeIngressostr = in.nextLine();
+
+			LocalDate dataDeIngresso = LocalDate.parse(dataDeIngressostr, formatter);
+
+			aluno[i] = new Aluno(nome, matricula, dataDeIngresso);
+
+		}
+	}
 
 }
